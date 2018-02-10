a4f9873c0d680a7cb653f813b5772bcfc2bb42c8
Kevin Louis <keviinlouiis@gmail.com>
2018-02-10 00:35:38 -0200

    ..

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -15,8 +15,6 @@ const state = {
 };
 
 const getters = {
-  isLoggedIn: state => state.status.logged,
-  pending: state => state.status.pending,
   user: state => state.user,
   followers: state => state.user.followers,
   following: state => state.user.following,
@@ -25,27 +23,6 @@ const getters = {
 };
 
 const mutations = {
-  login(state, {user, token}) {
-    localStorage.setItem('token', token);
-    localStorage.setItem('user', JSON.stringify(user));
-
-    state.token = token;
-    state.user = user;
-    state.status.logged = true;
-    state.status.pending = false;
-  },
-  logout(state) {
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
-
-    state.token = null;
-    state.status.logged = false;
-    state.user = {};
-  },
-  setToken(state, token) {
-    state.token = token;
-    state.status.logged = true;
-  },
   setUser(state, user) {
     localStorage.setItem('user', JSON.stringify(user));
     state.user = user;
@@ -69,52 +46,6 @@ const mutations = {
 };
 
 const actions = {
-  login({commit}, {email, password}) {
-    //Mostrando o spinner
-    state.status.pending = true;
-
-    return new Promise((resolve, reject) => {
-      axios.post('/auth/login', {email, password})
-        .then(response => {
-          commit('login', {user: response.data.user, token: response.data.token});
-          Axios.defaults.headers["Authorization"] = 'Bearer ' + localStorage.getItem('token');
-          resolve({user: response.data.user});
-        })
-        .catch(response => {
-          state.status.pending = false;
-          reject()
-        });
-    });
-
-  },
-  logout({commit, dispatch}) {
-    commit('logout');
-    dispatch('post/removePosts', {}, {root: true});
-  },
-  register({commit}, {name, email, password, password_confirmation}) {
-    //Mostrando o spinner
-    commit('logging');
-
-    return new Promise((resolve, reject) => {
-      axios.post('/auth/register', {name, email, password, password_confirmation})
-        .then(response => {
-          commit('login', {user: response.data.user, token: response.data.token});
-          resolve();
-        })
-        .catch(response => {
-          state.status.pending = false;
-          reject()
-        });
-    });
-  },
-  setToken({state, commit, dispatch}) {
-    let token = localStorage.getItem('token');
-    if (token && state.token === null) {
-      Axios.defaults.headers["Authorization"] = 'Bearer ' + localStorage.getItem('token');
-      commit('setToken', token);
-      dispatch('loadUser');
-    }
-  },
   loadUser({commit}) {
     let user = localStorage.getItem('user');
     if (user) {
